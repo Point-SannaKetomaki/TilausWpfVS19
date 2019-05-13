cfaf6654916454e157725a2352b71f1368bab2d0
Sanna Ketomaki <sanna.ketomaki@stud.pointcollege.fi>
2019-05-13 10:35:24 +0300

    Asiakaslistaus Drop-down tehty, tulostaa tietokannasta asiakas-ID:n käyttöliittymään

--- TilausAppWpfVS19/TilausAppWpfVS19/MainWindow.xaml.cs
+++ TilausAppWpfVS19/TilausAppWpfVS19/MainWindow.xaml.cs
@@ -23,11 +23,36 @@ namespace TilausAppWpfVS19
         public MainWindow()
         {
             InitializeComponent();
+            HaeAsiakkaat();  //Täytetään Asiakas-combobox
         }
 
-        private void CbAsiakkaat_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
 
+        private void HaeAsiakkaat()
+        {
+            List<cbPairAsiakas> cbPairAsiakkaat = new List<cbPairAsiakas>();
+            TilausDBEntities entities = new TilausDBEntities();
+
+            var asiakkaat = from a in entities.Asiakkaat
+                            select a;
+
+            foreach (var asiakas in asiakkaat)
+            {
+                cbPairAsiakkaat.Add(new cbPairAsiakas(asiakas.Nimi, asiakas.AsiakasID));
+            }
+
+            //Combo-box:n nimi on cbAsiakkaat
+            cbAsiakkaat.DisplayMemberPath = "asiakasNimi";  //listalla näkyy nimi, tulee cbPaisAsiakas.cs luokasta
+            cbAsiakkaat.SelectedValuePath = "asiakasNumero"; //tk:aan tallennettaessa käytetään as-numeroa, cbPairAsiakas.cs luokasta
+            cbAsiakkaat.ItemsSource = cbPairAsiakkaat;
+        }
+
+        private void CbAsiakkaat_DropDownClosed(object sender, EventArgs e)
+        {
+            cbPairAsiakas cbp = (cbPairAsiakas)cbAsiakkaat.SelectedItem;  //mikä asiakas valittiin listalta?
+            string AsiakasNimi = cbp.asiakasNimi;
+            int AsiakasNumero = cbp.asiakasNumero;
+            txtAsiakasNumero.Text = "";
+            txtAsiakasNumero.Text = AsiakasNumero.ToString(); //tulostetaan valitun asiakkaan as.numero
         }
     }
 }
---
+++ TilausAppWpfVS19/TilausAppWpfVS19/cbPairAsiakas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TilausAppWpfVS19
+{
+    class cbPairAsiakas
+    {
+        public string asiakasNimi { get; set; }
+        public int asiakasNumero { get; set; }
+        public cbPairAsiakas(string AsiakasNimi, int AsiakasNumero)
+        {
+            asiakasNimi = AsiakasNimi;
+            asiakasNumero = AsiakasNumero;
+        }
+    }
+}
